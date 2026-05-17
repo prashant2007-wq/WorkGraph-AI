a8a0ba038c6f662595b7502c11ca6d33f4e39402
PRASHANT S BISHT <prashantjee2025@gmail.com>
2026-05-17 22:15:19 +0530

    created dashboard layout

--- apps/frontend/src/layouts/DashboardLayout.tsx
+++ apps/frontend/src/layouts/DashboardLayout.tsx
@@ -1,17 +1,13 @@
-import { Outlet } from 'react-router-dom'
-import Sidebar from '../components/common/Sidebar'
-import Navbar from '../components/common/Navbar'
+import { Outlet } from "react-router-dom";
+import Sidebar from "../components/common/Sidebar";
 
 export default function DashboardLayout() {
   return (
-    <div className="flex h-screen bg-[#0F172A] overflow-hidden">
+    <div className="min-h-screen bg-slate-950 text-white flex">
       <Sidebar />
-      <div className="flex flex-col flex-1 overflow-hidden">
-        <Navbar />
-        <main className="flex-1 overflow-y-auto p-6">
-          <Outlet />
-        </main>
-      </div>
+      <main className="flex-1 p-8 overflow-y-auto">
+        <Outlet />
+      </main>
     </div>
-  )
+  );
 }
